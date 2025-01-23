9b3191b9582f2842a81ba6c8aa6eff1b63f91021
Lukas-12 <lukas.buchsi@gmail.com>
2025-01-23 12:05:46 +0100

    + add Options and audio mixer

--- RepCon/Assets/GameHandler.cs
+++ RepCon/Assets/GameHandler.cs
@@ -4,39 +4,65 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class GameHandler : MonoBehaviour{
 
         public static int playerStat1;
-        // public GameObject textGameObject;
+         public static bool GameisPaused = false;
+        public GameObject pauseMenuUI;
+        public AudioMixer mixer;
+        public static float volumeLevel = 1.0f;
+        private Slider sliderVolumeCtrl;
 
-        // void Start () { UpdateScore (); }
-
-        void Update(){
-        //NOTE: delete this quit functionality when a Pause Menu is added!
-                // if (Input.GetKey("escape")){
-                //         Application.Quit();
-                // }
-
+        void Awake(){
+                SetLevel (volumeLevel);
+                GameObject sliderTemp = GameObject.FindWithTag("PauseMenuSlider");
+                if (sliderTemp != null){
+                        sliderVolumeCtrl = sliderTemp.GetComponent<Slider>();
+                        sliderVolumeCtrl.value = volumeLevel;
+                }
+        }
+        void Start(){
+                pauseMenuUI.SetActive(false);
+                GameisPaused = false;
+        }
+       void Update(){
+                if (Input.GetKeyDown(KeyCode.Escape)){
+                        if (GameisPaused){ Resume(); }
+                        else{ Pause(); }
+                }
                 // Stat tester:
                 //if (Input.GetKey("p")){
                 //       Debug.Log("Player Stat = " + playerStat1);
                 //}
         }
 
-        // void UpdateScore () {
-        //        Text scoreTemp = textGameObject.GetComponent<Text>();
-        //        scoreTemp.text = "Score: " + score; }
+         public void Pause(){
+                pauseMenuUI.SetActive(true);
+                Time.timeScale = 0f;
+                GameisPaused = true;
+        }
+        public void Resume(){
+                pauseMenuUI.SetActive(false);
+                Time.timeScale = 1f;
+                GameisPaused = false;
+        }
+         public void SetLevel (float sliderValue){
+                mixer.SetFloat("MusicVolume", Mathf.Log10 (sliderValue) * 20);
+                volumeLevel = sliderValue;
+        }
 
         public void StartGame(){
                 SceneManager.LoadScene("Scene1");
         }
 
         public void OpenCredits(){
-                SceneManager.LoadScene("Credits");
+                SceneManager.LoadScene("CreditsScene");
         }
 
         public void RestartGame(){
+                Time.timeScale = 1f;
                 SceneManager.LoadScene("MainMenu");
         }
 
