8627a921fb46e29f6d34e8ba2ac1ddf1b51ea99c
Lukas-12 <lukas.buchsi@gmail.com>
2025-02-01 10:00:40 +0100

    + scene 1 dialogue

---
+++ RepCon/Assets/Scripts/Scene1Dialogue.cs
@@ -0,0 +1,96 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class Scene1Dialogue : MonoBehaviour {
+// These are the script variables.
+// For more character images / buttons, copy & renumber the variables:
+        public int primeInt = 1;        // This integer drives game progress!
+        public TMP_Text Char1name;
+        public TMP_Text Char1speech;
+        //public TMP_Text Char2name;
+        //public TMP_Text Char2speech;
+        //public TMP_Text Char3name;
+        //public TMP_Text Char3speech;
+        public GameObject DialogueDisplay;
+        public GameObject ArtChar1a;
+       //public GameObject ArtChar1b;
+       //public GameObject ArtChar1c;
+       //public GameObject ArtChar2;
+        public GameObject ArtBG1;
+        //public GameObject Choice1a;
+        //public GameObject Choice1b;
+        public GameObject NextScene1Button;
+        public GameObject NextScene2Button;
+        public GameObject nextButton;
+       //public AudioSource audioSource1;
+        private bool allowSpace = true;
+
+// Set initial visibility. Added images or buttons need to also be SetActive(false);
+        void Start(){  
+             DialogueDisplay.SetActive(false);
+             ArtChar1a.SetActive(false);
+             ArtBG1.SetActive(true);
+             //Choice1a.SetActive(false);
+             //Choice1b.SetActive(false);
+             NextScene1Button.SetActive(false);
+             NextScene2Button.SetActive(false);
+             nextButton.SetActive(true);
+        }
+
+// Use the spacebar as a faster "Next" button:
+        void Update(){        
+             if (allowSpace == true){
+                 if (Input.GetKeyDown("space")){
+                      Next();
+                 }
+
+                 // secret debug code: go back 1 Story Unit, if NEXT is visible
+                 if (Input.GetKeyDown("p")) {
+                      primeInt -= 2;
+                      Next();
+                 }
+             }
+        }
+
+//Story Units! The main story function.
+//Players hit [NEXT] to progress to the next primeInt:
+public void Next(){
+        primeInt += 1;
+        if (primeInt == 1){
+                // audioSource1.Play();
+        }
+        else if (primeInt == 2){
+                ArtChar1a.SetActive(true);
+                DialogueDisplay.SetActive(true);
+                Char1name.text = "Lotti";
+                Char1speech.text = "Hello There";
+                
+        }
+       else if (primeInt ==3){
+                Char1name.text = "Lotti";
+                Char1speech.text = "Whats up?";
+                //gameHandler.AddPlayerStat(1);
+        }
+       else if (primeInt == 4){
+                Char1name.text = "Lotti";
+                Char1speech.text = "Where i should go?";
+                nextButton.SetActive(false);
+                allowSpace = false;
+                NextScene1Button.SetActive(true);
+                NextScene2Button.SetActive(true);
+       }
+     }
+
+// FUNCTIONS FOR BUTTONS TO ACCESS (Choice #1 and SceneChanges)
+        public void SceneChange1(){
+               SceneManager.LoadScene("Scene2a");
+        }
+        public void SceneChange2(){
+                SceneManager.LoadScene("Scene2b");
+        }
+}
